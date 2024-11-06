33373eadb961d527dbedf9a54eb351c8db91d745
cazsky <lolcchz@gmail.com>
2024-11-06 20:20:36 +0800

    I HAVE FIGURED IT OUT

--- addons/gaea/plugin.gd
+++ addons/gaea/plugin.gd
@@ -3,6 +3,9 @@ extends EditorPlugin
 
 var _inspector_plugin = preload("./editor/inspector_plugin.gd")
 var _update_button = preload("./editor/update_button.tscn")
+var _better_terrain_changeset_renderer = preload("res://addons/better-terrain-in-gaea/better_terrain_changeset_renderer.gd.gd")
+var _better_terrain_gaea_renderer = preload("res://addons/better-terrain-in-gaea/better_terrain_gaea_renderer.gd")
+var _better_terrain_tile_info = preload("res://addons/better-terrain-in-gaea/better_terrain_tile_info.gd")
 
 
 func _enter_tree() -> void:
