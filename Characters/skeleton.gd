4ca9121384ce21d5a336474b17db76f60a0eea0d
cazsky <lolcchz@gmail.com>
2024-11-11 10:01:13 +0800

    Added raycasts to skeleton

--- Characters/skeleton.gd
+++ Characters/skeleton.gd
@@ -11,6 +11,7 @@ var can_attack: bool = true
 var distance_to_player: float
 
 @onready var attack_timer: Timer = $AttackTimer
+@onready var aim_raycast: RayCast2D = $AimRayCast
 
 
 
@@ -22,7 +23,8 @@ func _on_path_timer_timeout() -> void:
 		elif distance_to_player < MIN_DISTANCE_TO_PLAYER:
 			_get_path_to_move_away_from_player()
 		else:
-			if can_attack:
+			aim_raycast.target_position = player.position - global_position
+			if can_attack and state_machine.state == state_machine.states.idle and not aim_raycast.is_colliding():
 				can_attack = false
 				_throw_knife()
 				attack_timer.start()
