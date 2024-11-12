ccf63ad946348ec2f1e477af90184894bdc6d270
cazsky <lolcchz@gmail.com>
2024-11-12 21:35:32 +0800

    Added spikes to room0

--- Scenes/spikes.gd
+++ Scenes/spikes.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 func _collide(body: Node2D) -> void:
 	if not body.flying:
 		knockback_direction = (body.global_position - global_position).normalized()
-		body.take_damage(damage,knockback_force)
+		body.take_damage(damage,knockback_direction,knockback_force)
