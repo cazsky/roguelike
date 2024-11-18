908c341761d9698ae0cfd29bfd3eb9f1d4a999ea
cazsky <lolcchz@gmail.com>
2024-11-18 19:21:31 +0800

    Added charge attacks

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -4,6 +4,9 @@ extends Character
 @onready var sword: Node2D = get_node("Sword")
 @onready var sword_animation_player: AnimationPlayer = sword.get_node("SwordAnimationPlayer")
 @onready var sword_hitbox: Area2D = get_node("Sword/Node2D/SwordSprite/Hitbox")
+@onready var charge_particles: GPUParticles2D = $Sword/ChargeParticles
+
+
 
 func _process(_delta: float) -> void:
 	var mouse_direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
@@ -38,7 +41,12 @@ func get_input() -> void:
 	
 	#Sword Animation
 	if Input.is_action_just_pressed("ui_attack") and not sword_animation_player.is_playing():
-		sword_animation_player.play("attack")
+		sword_animation_player.play("charge")
+	elif Input.is_action_just_released("ui_attack"):
+		if sword_animation_player.is_playing() and sword_animation_player.current_animation == "charge":
+			sword_animation_player.play("attack")
+		elif charge_particles.emitting:
+			sword_animation_player.play("circular_attack")
 		
 func _ready() -> void:
 	set_collision_layer_value(2, true)
@@ -49,6 +57,8 @@ func switch_camera() -> void:
 	main_scene_camera.position = position
 	main_scene_camera.make_current()
 	
+func cancel_attack() -> void:
+	sword_animation_player.play("cancel_attack")
 	
 
 
