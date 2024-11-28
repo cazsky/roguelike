31c3400f3691bb9fa5a92c52c618fc4970df1186
cazsky <lolcchz@gmail.com>
2024-11-28 15:44:07 +0800

    Created room_with_weapon script to generate weapon randomly in room

--- Scripts/room.gd
+++ Scripts/room.gd
@@ -1,4 +1,5 @@
 extends Node2D
+class_name Room
 
 const SPAWN_EXPLOSION_SCENE: PackedScene = preload("res://Scenes/spawn_explosion.tscn")
 
---
+++ Scripts/room_with_weapon.gd
@@ -0,0 +1,11 @@
+extends Room
+
+const WEAPONS: Array = [preload("res://Weapons/axe.tscn"),preload("res://Weapons/sword.tscn")]
+
+@onready var weapon_pos: Marker2D = $WeaponPos
+
+func _ready() -> void:
+	var weapon: Node2D = WEAPONS.pick_random().instantiate()
+	weapon.position = weapon_pos.position
+	weapon.on_floor = true
+	add_child(weapon)
